fb2409b7339b34d1ece60aed99567890b0e2dc35
golden <josuagolden14@gmail.com>
2023-03-30 16:31:17 +0700

    tes simple onclick

--- app/src/main/java/com/example/tes_kotlin/LoginActivity.kt
+++ app/src/main/java/com/example/tes_kotlin/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.example.tes_kotlin
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tes_kotlin.R
 
@@ -10,5 +12,12 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginBt = findViewById<Button>(R.id.button_login)
+        val loginTxt = findViewById<TextView>(R.id.login_text)
+
+        loginBt.setOnClickListener() {
+            loginTxt.setText(R.string.login_button_clicked_text)
+        }
     }
 }
