98023ca71af108b2094ef193537d561db07d8a9a
golden <josuagolden14@gmail.com>
2023-03-30 16:22:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tes_kotlin/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.tes_kotlin
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tes_kotlin.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
